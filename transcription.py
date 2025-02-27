8f526c7b1415a1898bf9af7a5d47fac824838fd6
Kristian Hernandez <hernandez.cardenas10@gmail.com>
2025-02-26 22:25:48 -0600

    Optimized functions and added documentation

--- transcription.py
+++ transcription.py
@@ -1,33 +1,46 @@
-dna_transcription = {
+# Notes:
+# I chose to use generator expressions instead of list comprehension 
+# because genetic material strands can be very large, and using a 
+# generator helps optimize memory usage and improve performance.
+
+
+# This dictionary maps DNA nucleotides to their corresponding RNA bases
+dna_transcription_values = {
     "A" : "U",
     "T" : "A",
     "C" : "G",
     "G" : "C"
 }
-rna_transcription = {
+
+
+# This dictionary maps RNA nucleotides to their corresponding DNA bases
+rna_transcription_values = {
     "A" : "T",
-    "U" : "T",
+    "U" : "A",
     "C" : "G",
     "G" : "C" 
 }
 
+
+# Converts a DNA sequence into its corresponding RNA sequence.
+#
+# Parameters:
+# sequence (str): A string representing the DNA sequence.
+#
+# Returns:
+# str: The transcribed RNA sequence.
 def transcription(sequence):
 
-    rna_sequence = []   
-
-    for i in sequence:
-
-        rna_sequence.append(dna_transcription[i])
-
-    return "".join(rna_sequence)
+    return "".join(dna_transcription_values[i] for i in sequence)
 
 
-
+# Converts an RNA sequence back into its corresponding DNA sequence.
+#
+# Parameters:
+# sequence (str): A string representing the RNA sequence.
+#
+# Returns:
+# str: The reverse-transcribed DNA sequence.
 def reverse_transcription(sequence):
 
-    dna_sequence = []
-
-    for i in sequence:
-        dna_sequence.append(rna_transcription[i])
-
-    return "".join(dna_sequence)
+    return "".join(rna_transcription_values[i] for i in sequence)
